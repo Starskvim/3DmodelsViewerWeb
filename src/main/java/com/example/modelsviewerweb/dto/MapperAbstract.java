f3ed2b8bf14074e2f1d051994c84258284ff0e5f
Mikhail Gaivola <starskvim@mail.ru>
2022-03-02 21:09:17 +0300

    Updated

--- src/main/java/com/example/modelsviewerweb/dto/MapperAbstract.java
+++ src/main/java/com/example/modelsviewerweb/dto/MapperAbstract.java
@@ -4,12 +4,15 @@ import com.example.modelsviewerweb.entities.PrintModelWeb;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+
 import java.util.List;
 
 @Mapper
 public abstract class MapperAbstract {
 
-    @Mapping(target = "previewOth", expression = "java(printModelWeb.getPreviewModel().getPreviewOth())")
     public abstract List<PrintModelPreviewDto> toPrintModelsPreviewDto(List<PrintModelWeb> printModelWeb);
+
+    @Mapping(target = "previewOth",  source = "printModelWeb.previewModel.previewOth")
+    public abstract PrintModelPreviewDto toPrintModelsPreviewDto(PrintModelWeb printModelWeb);
 
 }
